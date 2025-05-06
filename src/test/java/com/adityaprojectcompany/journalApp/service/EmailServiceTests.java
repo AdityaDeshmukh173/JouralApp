de5af2f9ecce471ab70146498513ca707815331f
AdityaDeshmukh173 <115912608+AdityaDeshmukh173@users.noreply.github.com>
2025-05-07 00:13:04 +0530

    target file removed from gitignore for jar file

--- src/test/java/com/adityaprojectcompany/journalApp/service/EmailServiceTests.java
+++ src/test/java/com/adityaprojectcompany/journalApp/service/EmailServiceTests.java
@@ -1,17 +1,17 @@
-package com.adityaprojectcompany.journalApp.service;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class EmailServiceTests {
-
-    @Autowired
-    private EmailService emailService ;
-
-    @Test
-    void TestSendMail(){
-        emailService.sendEmail("amyourmf2@gmail.com", "Testing Java Mail Sender", "Hii How are you Darling !??");
-    }
-}
+//package com.adityaprojectcompany.journalApp.service;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class EmailServiceTests {
+//
+//    @Autowired
+//    private EmailService emailService ;
+//
+//    @Test
+//    void TestSendMail(){
+//        emailService.sendEmail("amyourmf2@gmail.com", "Testing Java Mail Sender", "Hii How are you Darling !??");
+//    }
+//}
